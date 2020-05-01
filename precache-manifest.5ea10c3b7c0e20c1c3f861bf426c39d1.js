c8eb11a73cfcc720712e49a77dc07a175605d94d
amanjain4103 <amanjain4103@gmail.com>
2020-05-01 13:07:44 +0530

    Updates

---
+++ precache-manifest.5ea10c3b7c0e20c1c3f861bf426c39d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f068c16e4b6bbd3981ef2ecbf825870",
+    "url": "/imagerecog/index.html"
+  },
+  {
+    "revision": "eb5f4e1032444b63e90b",
+    "url": "/imagerecog/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "429786a3b30bebc03493",
+    "url": "/imagerecog/static/css/main.87c45dd5.chunk.css"
+  },
+  {
+    "revision": "eb5f4e1032444b63e90b",
+    "url": "/imagerecog/static/js/2.d6100127.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imagerecog/static/js/2.d6100127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429786a3b30bebc03493",
+    "url": "/imagerecog/static/js/main.b2d36084.chunk.js"
+  },
+  {
+    "revision": "ada713699158fd0638bf",
+    "url": "/imagerecog/static/js/runtime-main.8e6a8fe2.js"
+  },
+  {
+    "revision": "46d295667b9240365330ce3949d514de",
+    "url": "/imagerecog/static/media/logo1.46d29566.png"
+  }
+]);
